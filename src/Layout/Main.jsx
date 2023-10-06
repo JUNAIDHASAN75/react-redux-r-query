a0a246c5371201cd81a76cc484ce8f84a66a916d
Junaid Hasan <spiderss907@gmail.com>
2023-10-07 00:14:18 +0600

    navbar & home page added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import Home from "../Pages/Home/Home/Home";
+import NavBar from "../Pages/Shared/Navbar/Navbar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Home></Home>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Pages/Shared/Navbar/NavBar.jsx
@@ -0,0 +1,27 @@
+import {  Container, Nav,Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
+const NavBar = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary my-4">
+      <Container className="d-flex justify-content-between">
+        <h2>Redux</h2>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="flex-grow-0" id="basic-navbar-nav ">
+          <Nav className="me-auto d-flex gap-4 align-items-md-center ">
+            <Link className="text-decoration-none h5" to='/'>Home</Link>
+            <Link className="text-decoration-none h5" to='/'>Protected Route</Link>
+            <Link className="text-decoration-none h5" to='/'>About</Link>
+            <Link className="text-decoration-none h5" to='/'>Contact Us</Link>
+            <div>
+                <Link className="btn btn-primary">Login</Link>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
+};
+
+export default NavBar;
