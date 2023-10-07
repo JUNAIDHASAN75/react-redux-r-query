98fafa5691169db0bf448eec372fb9026ef4b3af
Junaid Hasan <spiderss907@gmail.com>
2023-10-07 19:14:33 +0600

    added mutation and postapi

--- src/Pages/AddCoffee/AddCoffee.jsx
+++ src/Pages/AddCoffee/AddCoffee.jsx
@@ -1,9 +1,49 @@
+import { useForm } from "react-hook-form";
+import {  useGetApiQuery, usePostApiMutation } from "../../features/slices/apis/apiSlice";
 
 
 const AddCoffee = () => {
+    const { register, handleSubmit} = useForm();
+    const {data} = useGetApiQuery()
+    console.log(data)
+    const [postApi, {data:coffees}] = usePostApiMutation()
+    console.log(coffees)
+    const onSubmit = data =>{
+        postApi(data)
+         console.log(data)
+        };
     return (
         <div>
-            <h2>add coffee</h2>
+            <h2>Add Coffee</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="photo " className="my-2">
+                        <span className="h5">Name</span>
+                    </label>
+                    <input type="text"className="p-3 rounded-1 border-2 w-100" {...register('name')} placeholder="Pame" />
+                </div>
+                <div>
+                    <label htmlFor="photo " className="my-2">
+                        <span className="h5">Price</span>
+                    </label>
+                    <input type="text"className="p-3 rounded-1 border-2 w-100" {...register('price')} placeholder="Price" />
+                </div>
+                <div>
+                    <label htmlFor="photo " className="my-2">
+                        <span className="h5">Photo</span>
+                    </label>
+                    <input type="text"className="p-3 rounded-1 border-2 w-100" {...register('photo')} placeholder="PhotoURL" />
+                </div>
+                <div>
+                    <label htmlFor="photo " className="my-2">
+                        <span className="h5">Details</span>
+                    </label>
+                    <input type="text"className="p-3 rounded-1 border-2 w-100" {...register('details')} placeholder="Details" />
+                </div>
+                <div className="my-4">
+                    <input className="btn btn-primary" type="submit" value="Add Coffee" />
+                </div>
+            </form>
         </div>
     );
 };
